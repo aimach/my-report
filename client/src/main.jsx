45ef397b44fc3b61e8d700dd556ded2d8eae5580
Marie-Hermine Schneider <vigneron.mh@gmail.com>
2024-05-25 14:16:49 +0200

    Feature (route): add basic frontend routes structure

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,27 +3,33 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-  },
-  {
-    path: "/create",
-    element: <div>Page de création d&apos;un compte-rendu</div>,
-  },
-  {
-    path: "/dashboard",
-    element: <div>Page du dashboard du directeur commercial</div>,
-  },
-  {
-    path: "/login",
-    element: <div>Page de login</div>,
-  },
-  {
-    path: "/register",
-    element: <div>Page de register</div>,
+    children: [
+      {
+        path: "create",
+        element: <div>Page de création d&apos;un compte-rendu</div>,
+      },
+      {
+        path: "dashboard",
+        element: <div>Page du dashboard du directeur commercial</div>,
+      },
+      {
+        path: "login",
+        element: <div>Page de login</div>,
+      },
+      {
+        path: "register",
+        element: <div>Page de register</div>,
+      },
+    ],
   },
 ]);
 
