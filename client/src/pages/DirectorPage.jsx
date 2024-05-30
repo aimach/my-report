bdfd838b6fe171b5faf90b18a4ed80a0ee037281
Marie-Hermine Schneider <vigneron.mh@gmail.com>
2024-05-30 08:15:07 +0200

    Feature (auth): add protection and redirect on director page

--- client/src/pages/DirectorPage.jsx
+++ client/src/pages/DirectorPage.jsx
@@ -1,48 +1,63 @@
-import { useContext, useEffect, useState } from "react";
-import { deleteVisit, getAllVisitsWithCommercialId } from "../services/visits";
-import connexion from "../services/connexion";
+import { useContext, useEffect } from "react";
 
-import { TableSortLabel, Typography, Container } from "@mui/material";
+import { Typography, Container } from "@mui/material";
 
 import { AuthContext } from "../context/authContext";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLoaderData } from "react-router-dom";
+import SalesChart from "../components/SalesChart.jsx";
 
 function DirectorPage() {
   const { profile } = useContext(AuthContext);
   const navigate = useNavigate();
+
+  const chartStats = useLoaderData();
+  const series = [];
+  chartStats.map((chart) => {
+    const { commercialFirstname, commercialLastName } = chart[0];
+    const serie = chart.map((item) => ({
+      year: item.year,
+      data: item.data,
+    }));
+    series.push({ commercialFirstname, commercialLastName, serie });
+  });
 
   useEffect(() => {
     // on redirige l'utilisateur s'il n'est pas directeur
     if (profile && profile.is_director === false) {
       navigate("/");
     }
-  }, [profile, navigate]);
+  }, [profile]);
 
   return (
-    <Container
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        pb: { xs: 8, sm: 12 },
-      }}
-    >
-      <Typography
-        variant="h2"
+    chartStats && (
+      <Container
         sx={{
           display: "flex",
-          flexDirection: { xs: "column", md: "row" },
-          alignSelf: "center",
-          textAlign: "center",
-          fontSize: "clamp(3.5rem, 10vw, 4rem)",
-          pb: { xs: 8, sm: 8 },
-          fontWeight: 500,
+          flexDirection: "column",
+          alignItems: "center",
+          pb: { xs: 8, sm: 12 },
         }}
-        color="primary"
       >
-        Dashboard
-      </Typography>
-    </Container>
+        <Typography
+          variant="h2"
+          sx={{
+            display: "flex",
+            flexDirection: { xs: "column", md: "row" },
+            alignSelf: "center",
+            textAlign: "center",
+            fontSize: "clamp(3.5rem, 10vw, 4rem)",
+            pb: { xs: 8, sm: 8 },
+            fontWeight: 500,
+          }}
+          color="primary"
+        >
+          Dashboard
+        </Typography>
+        <Container maxWidth="xl">
+          <SalesChart series={series} />
+        </Container>
+      </Container>
+    )
   );
 }
 
