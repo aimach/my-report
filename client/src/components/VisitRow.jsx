1ad38cea8c1c6001a4c0b388698fb4a95313dacb
Marie-Hermine Schneider <vigneron.mh@gmail.com>
2024-05-30 11:49:50 +0200

    Feature (visits list): add report content in a tooltip

--- client/src/components/VisitRow.jsx
+++ client/src/components/VisitRow.jsx
@@ -1,71 +1,82 @@
 import { format, isFuture } from "date-fns";
+import { useNavigate } from "react-router-dom";
 import TableCell from "@mui/material/TableCell";
 import TableRow from "@mui/material/TableRow";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import { Chip, IconButton, Tooltip } from "@mui/material";
-import { useNavigate } from "react-router-dom";
 
 function VisitRow({ visit, handleDeleteButton }) {
   const navigate = useNavigate();
   return (
-    <TableRow
-      key={visit._id}
-      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+    <Tooltip
+      arrow
+      enterDelay={200}
+      placement="top"
+      title={
+        <>
+          <p>{visit.report_content.substring(0, 500)}</p>
+        </>
+      }
     >
-      <TableCell component="th" scope="row">
-        {isFuture(visit.date) ? (
-          <Chip label="A venir" color="success" variant="outlined" />
-        ) : (
-          <Chip label="Passée" color="primary" variant="outlined" />
-        )}
-      </TableCell>
-      <TableCell component="th" scope="row" align="center">
-        {format(visit.date, "dd/MM/yyyy")}
-      </TableCell>
-      <TableCell component="th" scope="row" align="center">
-        {visit.client.lastname} {visit.client.firstname}
-      </TableCell>
-      <TableCell component="th" scope="row" align="center">
-        {visit.article.name}
-      </TableCell>
-      <TableCell component="th" scope="row" align="right">
-        {visit.article_nb}
-      </TableCell>
-      <TableCell component="th" scope="row" align="right">
-        {visit.sales} €
-      </TableCell>
-      <TableCell component="th" scope="row" align="center">
-        <Tooltip title="Voir">
-          <IconButton
-            color="primary"
-            aria-label="see-visit"
-            onClick={() => navigate(`/visit/${visit._id}/see`)}
-          >
-            <VisibilityIcon />
-          </IconButton>
-        </Tooltip>
-        <Tooltip title="Modifier">
-          <IconButton
-            color="primary"
-            aria-label="edit-visit"
-            onClick={() => navigate(`/visit/${visit._id}/modify`)}
-          >
-            <EditIcon />
-          </IconButton>
-        </Tooltip>
-        <Tooltip title="Supprimer">
-          <IconButton
-            color="error"
-            aria-label="delete-visit"
-            onClick={() => handleDeleteButton(visit._id)}
-          >
-            <DeleteIcon />
-          </IconButton>
-        </Tooltip>
-      </TableCell>
-    </TableRow>
+      <TableRow
+        key={visit._id}
+        sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+      >
+        <TableCell component="th" scope="row">
+          {isFuture(visit.date) ? (
+            <Chip label="A venir" color="success" variant="outlined" />
+          ) : (
+            <Chip label="Passée" color="primary" variant="outlined" />
+          )}
+        </TableCell>
+        <TableCell component="th" scope="row" align="center">
+          {format(visit.date, "dd/MM/yyyy")}
+        </TableCell>
+        <TableCell component="th" scope="row" align="center">
+          {visit.client.lastname} {visit.client.firstname}
+        </TableCell>
+        <TableCell component="th" scope="row" align="center">
+          {visit.article.name}
+        </TableCell>
+        <TableCell component="th" scope="row" align="right">
+          {visit.article_nb}
+        </TableCell>
+        <TableCell component="th" scope="row" align="right">
+          {visit.sales} €
+        </TableCell>
+        <TableCell component="th" scope="row" align="center">
+          <Tooltip title="Voir">
+            <IconButton
+              color="primary"
+              aria-label="see-visit"
+              onClick={() => navigate(`/visit/${visit._id}/see`)}
+            >
+              <VisibilityIcon />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Modifier">
+            <IconButton
+              color="primary"
+              aria-label="edit-visit"
+              onClick={() => navigate(`/visit/${visit._id}/modify`)}
+            >
+              <EditIcon />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Supprimer">
+            <IconButton
+              color="error"
+              aria-label="delete-visit"
+              onClick={() => handleDeleteButton(visit._id)}
+            >
+              <DeleteIcon />
+            </IconButton>
+          </Tooltip>
+        </TableCell>
+      </TableRow>
+    </Tooltip>
   );
 }
 export default VisitRow;
